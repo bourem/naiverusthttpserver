6eae510197bba199db0d99364f210a785024ac1c
RB <remi.bouchez@avep.io>
2017-04-11 20:56:50 +0900

    1. Add expects to handle errors 2. Use BufReader with read_line, instead of read_to_string, since it prevents the communication from hanging after the read
    Note: had to clone the stream, since BufRead gives ownership to it
    Note: ostream.shutdown fails, not sure why

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,28 @@
 use std::net::{TcpListener, TcpStream, Shutdown};
 use std::io::prelude::*;
+use std::io;
 
 fn main() {
 
-    let listener = TcpListener::bind("127.0.0.1:8000").unwrap();
+    let listener = TcpListener::bind("127.0.0.1:8000").expect("failed to start server");
 
-    fn handle_client(mut stream: TcpStream) {
+    fn handle_client(stream: TcpStream) {
         println!("Hello, world!");
-        let mut request = [0;10];
-        let a = stream.read(&mut request);
+
+        let mut ostream = stream.try_clone().expect("couldn't clone");
+        let mut reader = io::BufReader::new(stream);
+        let mut request = String::new();
+
+        let a = reader.read_line(&mut request).expect("read failed");
         println!("{:?} - {:?}", a, request);
-        let b = stream.write(b"HTTP/1.0 200 OK\nContent-Type: text/html\n\n<html><body>Hello there!</body></html>");
-        println!("{:?}", b);
-        stream.flush();
-        stream.shutdown(Shutdown::Both);
+        
+        ostream.write_all(b"HTTP/1.0 200 OK\nContent-Type: text/html\n\n<html><body>Hello there!</body></html>").expect("write failed");
+
+        ostream.flush().expect("stream flushed");
+        match ostream.shutdown(Shutdown::Both) {
+            Ok(_) => println!("shutdown call successful"),
+            Err(e) => println!("shutdown call failed, with error: {:?}", e)
+        };
     }
 
     for stream in listener.incoming() {
